421e2c82a3a139bac662919eec5a237d36cc24ba
Ezio7777 <sunitpal2000@gmail.com>
2024-01-23 21:43:15 +0530

    add market component

--- Frontend/src/components/market.jsx
+++ Frontend/src/components/market.jsx
@@ -1,10 +1,18 @@
 import React from "react";
 import Market_Nav from "./market_Nav";
+import Card from "./market_card";
+import "../styles/market.css";
 
 function market() {
   return (
     <>
       <Market_Nav />
+      <div className="container">
+        <h1 className="market_header">Vegetables</h1>
+        <div>
+          <Card />
+        </div>
+      </div>
     </>
   );
 }
---
+++ Frontend/src/components/market_card.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "../styles/market_card.css";
+
+function market_card() {
+  return (
+    <div className="market_card">
+      <p>Onion</p>
+    </div>
+  );
+}
+
+export default market_card;
